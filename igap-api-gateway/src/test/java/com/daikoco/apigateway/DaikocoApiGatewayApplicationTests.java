e264ebd5bfe95602dcb5ca6e13e077d0b824a8b6
milad <a.aminzade1987@gmail.com>
2024-05-05 19:34:26 +0330

    API Gateway

---
+++ igap-api-gateway/src/test/java/com/daikoco/apigateway/DaikocoApiGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.daikoco.apigateway;
+
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikocoApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() throws JSONException {
+    }
+
+}
